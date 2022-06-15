376a737bbf8d9216db78421912984f898ecd34e0
martcube <martingjorceski@gmail.com>
2022-06-15 13:58:58 +0200

    intro height

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
 		htmlAttrs: {
 			lang: 'en',
 		},
-		meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }, { hid: 'description', name: 'description', content: '' }, { name: 'format-detection', content: 'telephone=no' }],
+		meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'user-scalable=no, width=device-width, height=device-height' }, { hid: 'description', name: 'description', content: '' }, { name: 'format-detection', content: 'telephone=no' }],
 		link: [{ rel: 'icon', type: 'png', href: '/favicon.png' }],
 	},
 
